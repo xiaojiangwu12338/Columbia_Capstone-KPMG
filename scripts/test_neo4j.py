6c203a50ac813fcbff83b21be176311eb741c097
ruoheng-du <rd2910@nyu.edu>
2025-10-03 00:48:57 -0400

    add docker setup and basic Neo4j test scripts

---
+++ scripts/test_neo4j.py
@@ -0,0 +1,41 @@
+'''
+####
+Steps
+1. Install Docker:
+https://www.docker.com/products/docker-desktop/
+
+2. Start the database
+```
+cd docker
+docker compose up -d
+```
+Then visit: http://localhost:7474
+
+3. Test connection
+```
+cd ..
+python scripts/test_neo4j.py
+```
+
+4. Shut down the database
+```
+docker compose down
+```
+'''
+
+# test if neo4j connected
+from neo4j import GraphDatabase
+import os
+
+# load from .env (can use python-dotenv to automatically load .env in the future)
+uri = os.getenv("NEO4J_URI", "bolt://localhost:7687")
+user = os.getenv("NEO4J_USERNAME", "neo4j")
+password = os.getenv("NEO4J_PASSWORD", "healthragkpmg")
+
+driver = GraphDatabase.driver(uri, auth=(user, password))
+
+with driver.session() as session:
+    result = session.run("RETURN 'Neo4j connected!' AS msg")
+    print(result.single()["msg"])
+
+driver.close()
